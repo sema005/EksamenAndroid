ab9e4d3ad62ba546e0082f4b7cab1449e557f231
Mathias223 <mata003@student.kristiania.no>
2022-04-08 15:01:25 +0200

    Fragments not working

--- app/src/main/java/no/kristiania/eksamenandroid/MainActivity.kt
+++ app/src/main/java/no/kristiania/eksamenandroid/MainActivity.kt
@@ -2,10 +2,29 @@ package no.kristiania.eksamenandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import no.kristiania.eksamenandroid.fragments.adapters.viewPagerAdapter
+import no.kristiania.eksamenandroid.fragments.reverseImageSearchResults
+import no.kristiania.eksamenandroid.fragments.saveResults
+import no.kristiania.eksamenandroid.fragments.selectUplodeShowimage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_first)
+        setContentView(R.layout.activity_main)
+
+
+        setUpTabs()
+    }
+
+    private fun setUpTabs(){
+        val adapter = viewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(reverseImageSearchResults,"Reverse Image Search Result")
+        adapter.addFragment(saveResults, "save result")
+        adapter.addFragment(selectUplodeShowimage, "select Uplode IMG")
+        viewPager.adapter = adapter
+        tabs.setupWithVewpager(viewPager)
+
+        tabs.getTabAT
+
     }
 }
